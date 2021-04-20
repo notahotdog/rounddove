b270e14e7046e2d8fbea70634d1b0f4b0a4d481a
Notahotdog-Umar Moiz <umarmoiz@gmail.com>
2021-04-20 22:34:03 +0800

    fixed routing issue

--- src/TopNavBar.js
+++ src/TopNavBar.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Menu, Button } from "antd";
 import { UserOutlined, AppstoreOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
 
 export default class TopNavBar extends Component {
   loadVote = () => {
@@ -12,12 +13,13 @@ export default class TopNavBar extends Component {
     return (
       <div>
         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
-          <Menu.Item icon={<AppstoreOutlined />}>Main Page</Menu.Item>
+          <Menu.Item icon={<AppstoreOutlined />}>
+            <Link to="/">Main Page</Link>
+          </Menu.Item>
           <Menu.Item icon={<UserOutlined />}>User Data</Menu.Item>
-          <Menu.Item icon={<UserOutlined />}> Facilitator</Menu.Item>
-          {/* <Button type="primary" onClick={() => this.loadVote()}>
-            Vote Test
-          </Button> */}
+          <Menu.Item icon={<UserOutlined />}>
+            <Link to="/FacilitatorPage">Facilitator</Link>
+          </Menu.Item>
         </Menu>
       </div>
     );
---
+++ src/UserPage.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import "./App.css";
+import "antd/dist/antd.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import TopNavBar from "./TopNavBar";
+import QuestionPage from "./QuestionPage";
+import { Layout } from "antd";
+import { Carousel } from "antd";
+
+// const { Title } = Typography;
+const { Content } = Layout; //Dereferencing
+
+export default class UserPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "jared",
+      jsonData: [],
+      questions: [
+        "Hazard: Thermal Runaway, what is the cause?",
+        "Hazard: Thermal Runaway, what are some additional controls that should be put inplace ?",
+        "Hazard: Thermal Runaway, do you have any additional comments?",
+      ],
+    };
+    this.generatePage = this.generatePage.bind(this);
+  }
+
+  componentDidMount() {
+    // console.log("App.js componentDidMount Called");
+    const userName = window.prompt("Username", "Anonymous"); //May need to change this into a form later
+    this.setState({ username: userName });
+    // Get Json Data
+    // 1. Question Type - Remarks/Voting
+    // 2. Question - What is the question you need feedback on
+    // 3. If Voting - What are the options available
+  }
+
+  //Dynamically generates the page
+  generatePage = (question) => {
+    return (
+      <QuestionPage
+        key={question}
+        username={this.state.username}
+        question={question}
+      />
+    );
+  };
+
+  render() {
+    // console.log("App.js Render called");
+
+    let questionList = this.state.questions;
+
+    return (
+      <div className="App">
+        <Layout style={{ height: "100vh" }}>
+          <TopNavBar />
+          <Content>
+            <Carousel dotPosition={"top"}>
+              <QuestionPage username={this.state.username} />
+              {questionList.map((qs) => this.generatePage(qs))}
+            </Carousel>
+          </Content>
+        </Layout>
+      </div>
+    );
+  }
+}
