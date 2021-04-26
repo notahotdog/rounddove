5d91b0a2be5b2499c901b015e7ae12cd7b7c159c
Notahotdog-Umar Moiz <umarmoiz@gmail.com>
2021-04-26 14:56:04 +0800

    fix chatbox css issue

--- src/FacilitatorChatList.js
+++ src/FacilitatorChatList.js
@@ -10,9 +10,13 @@ var ChatList = ({ chats }) => (
       return (
         <div>
           <div className="card">
-            <div className="card-user-details">{chat.username}</div>
-            <div className="card-user-comments">{chat.message}</div>
-            <div className="card-action-bar">
+            <div className="card-header-wrapper">
+              <div className="card-username">{chat.username} </div>
+            </div>
+            <div className="card-body-wrapper">
+              <div className="card-user-feedback">{chat.message}</div>
+            </div>
+            <div className="card-footer-wrapper">
               <Button
                 type="primary"
                 shape="round"
@@ -29,60 +33,28 @@ var ChatList = ({ chats }) => (
                   // fontSize: "0.5em",
                 }}
               />
+              <Button
+                type="primary"
+                shape="round"
+                icon={<CloseOutlined style={{ fontSize: "10px" }} />}
+                size="small"
+                style={{
+                  background: "red",
+                  borderColor: "red",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  height: "1.5em",
+                  width: "2em",
+                  // fontSize: "0.5em",
+                }}
+              />
             </div>
           </div>
-          {/* <Card
-            type="inner"
-            title={<Title level={5}>{chat.username}</Title>}
-            hoverable={"true"}
-            bordered={"true"}
-            borderRadius={5}
-            style={{ width: 300 }}
-          >
-            {chat.message}
-            <Button
-              type="primary"
-              shape="round"
-              icon={<CheckOutlined style={{ fontSize: "10px" }} />}
-              size="small"
-              style={{
-                // background: "red",
-                // borderColor: "yellow",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "1.5em",
-                width: "2em",
-                // fontSize: "0.5em",
-              }}
-            />
-            <Button
-              type="danger"
-              shape="round"
-              icon={<CloseOutlined style={{ fontSize: "10px" }} />}
-              size="small"
-              style={{
-                border: "2px solid purple",
-                // background: "red",
-                // borderColor: "yellow",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "1.5em",
-                width: "2em",
-                // fontSize: "0.5em",
-              }}
-            ></Button>
-          </Card> */}
-          {/* <Title level={2}>Subject Heading </Title>
-          <p>{chat.message}</p>
-          <Meta title="Page 1" description={chat.message} /> */}
         </div>
       );
     })}
   </div>
-
-  // <ul>
 );
 
 export default ChatList;
