2d9f8c7cedc54a2a9219731584aac7d2e7f68d82
Notahotdog-Umar Moiz <umarmoiz@gmail.com>
2021-05-06 16:40:55 +0800

    added logic for modal

--- src/WorkshopCreatePage.js
+++ src/WorkshopCreatePage.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Breadcrumb, Layout, Typography } from "antd";
 import { InputNumber } from "antd";
-import { Form, Input, Button, Checkbox } from "antd";
+import { Form, Input, Button, Checkbox, Search, Switch } from "antd";
 import { Modal } from "antd";
 
 const { Header, Footer, Sider, Content } = Layout;
@@ -12,14 +12,27 @@ export default class WorkshopCreatePage extends Component {
     super(props);
     this.state = {
       ModalText: "Content of the modal",
+      questionList: [],
       visible: false,
       confirmLoading: false,
+      question: "", //question to be added
+      workShopData: [], // an array of the all the different topics
+      isVoteChecked: false, //checks if the vote option is triggered
     };
     this.goBack = this.goBack.bind(this); // i think you are missing this
     this.onChange = this.onChange.bind(this);
     this.handleOk = this.handleOk.bind(this);
     this.handleCancel = this.handleCancel.bind(this);
+    this.updateTopicName = this.updateTopicName.bind(this);
+    this.updateQuestion = this.updateQuestion.bind(this);
+    this.addToQuestionList = this.addToQuestionList.bind(this);
+    this.changeQuestionType = this.changeQuestionType.bind(this);
     // this.showModal = this.showModal.bind(this);
+  }
+
+  //When first Mounted
+  componentDidMount() {
+    console.log("Create New Workshop");
   }
 
   //Returns the back to the prev dir
@@ -58,9 +71,31 @@ export default class WorkshopCreatePage extends Component {
     });
   };
 
-  render() {
-    console.log("Create New Workshop");
+  //Handles topic Name
+  updateTopicName = (e) => {
+    this.setState({ topicName: e.target.value });
+    console.log("Update Topic Name:", e.target.value);
+  };
 
+  //updates parameter within the question
+  updateQuestion = (e) => {
+    this.setState({ question: e.target.value });
+    console.log("Update Question:", e.target.value);
+  };
+
+  addToQuestionList = () => {
+    this.setState({
+      questionList: [...this.state.questionList, this.state.question],
+    });
+  };
+
+  //Changes the question Type from vote to normal vice versa
+  changeQuestionType = () => {
+    this.setState({ isVoteChecked: !this.state.isVoteChecked });
+    console.log("Changing Question Type");
+  };
+
+  render() {
     const onFinish = (values) => {
       console.log("Success:", values);
     };
@@ -122,18 +157,59 @@ export default class WorkshopCreatePage extends Component {
             <Button type="primary" onClick={this.showModal}>
               Add Topic
             </Button>
-            <Modal
-              title="Create A New Topic"
-              visible={visible}
-              onOk={this.handleOk}
-              confirmLoading={confirmLoading}
-              onCancel={this.handleCancel}
-            >
-              <Title level={5}>Topic Name</Title>
-              <Input placeholder="Type in the Topic name" />
-
-              <p>{ModalText}</p>
-            </Modal>
+            <div className="modal">
+              <Modal
+                title="Create A New Topic"
+                visible={visible}
+                onOk={this.handleOk}
+                confirmLoading={confirmLoading}
+                onCancel={this.handleCancel}
+              >
+                <div className="topic-box">
+                  <Title level={4}>Topic: {this.state.topicName}</Title>
+                  <Input
+                    placeholder="Type in the Topic name"
+                    onChange={this.updateTopicName}
+                  />{" "}
+                </div>
+                <div className="question-box">
+                  <br />
+                  <Title level={5}>Question: {this.state.question}</Title>
+                  <div
+                    className="question-type"
+                    style={{ display: "flex", flexDirection: "row" }}
+                  >
+                    <p>Type: </p>{" "}
+                    <Switch
+                      checkedChildren="vote"
+                      unCheckedChildren="normal"
+                      style={{ marginLeft: "5px" }}
+                      onClick={this.changeQuestionType}
+                    />
+                  </div>
+                  <div
+                    className="question-input"
+                    style={{ display: "flex", flexDirection: "row" }}
+                  >
+                    <Input
+                      placeholder="Type in the Question"
+                      onChange={this.updateQuestion}
+                    />{" "}
+                    <Button
+                      type="primary"
+                      size="medium"
+                      onClick={this.addToQuestionList}
+                    >
+                      Add Question
+                    </Button>
+                  </div>
+                </div>
+                {/* TODO - Dynamic render of the question array*/}
+                <br />
+                <h3> List of questions</h3>
+                {this.state.questionList}
+              </Modal>
+            </div>
           </Form>
         </Content>
 
